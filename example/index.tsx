86ebc80e99f2cd57d6422b985e9be4ae0b107d59
thejsj <jorge.silva@thejsj.com>
2026-02-02 11:16:20 -0500

    Add type keyword to TypeScript type-only imports

--- example/index.tsx
+++ example/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { EmojiPicker } from '../.';
+import EmojiPicker from '../src/index';
 
 const App = () => {
+  const [emojis, setEmojis] = React.useState('');
+  const [filterString, setFilterString] = React.useState('');
   return (
     <div>
-      <EmojiPicker />
+      <h2>Emojis: {emojis}</h2>
+      <input type="text" value={filterString} onChange={(e) => setFilterString(e.target.value)} />
+      <EmojiPicker onEmojiClick={(e) => setEmojis(emojis + e.emoji)} searchDisabled={false}/>
     </div>
   );
 };
